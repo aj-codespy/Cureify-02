9b0e3276f90160fce18002d81c9d6cf2af969261
Ayush Jha <145047690+aj-codespy@users.noreply.github.com>
2025-03-29 10:39:53 +0530

    Update imgQuery.py

--- imgQuery.py
+++ imgQuery.py
@@ -7,7 +7,7 @@ genai.configure(api_key='AIzaSyDtB4bETfNDyvpzA_NnBKMrr56rdiOE8bQ')
 def imgQuery(img, prompt):
     sample_file_1 = PIL.Image.open(img)
 
-    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash")
 
     basePrompt = "You are a medical specialist and your task is to answer the query based on the given image and the prompt you have to answer the user's medical query. And if it is not a medical thing then just answer it is not a valid medical input."
 
