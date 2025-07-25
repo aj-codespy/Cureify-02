799949a0092972ebca43b9b7f3b01eec5dfbd3cb
Ayush Jha <145047690+aj-codespy@users.noreply.github.com>
2025-07-25 11:49:33 +0530

    Update woundAnalysis.py

--- woundAnalysis.py
+++ woundAnalysis.py
@@ -1,7 +1,7 @@
 import PIL.Image
 import os
 import google.generativeai as genai
-genai.configure(api_key='AIzaSyDtB4bETfNDyvpzA_NnBKMrr56rdiOE8bQ')
+genai.configure(api_key='AIzaSyBozQi2V59ZCzUI6smDyDHt1j9sSSkcZbE')
 
 def woundAnalysis(img, prompt):
     basePrompt = '''
